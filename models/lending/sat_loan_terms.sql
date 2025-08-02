0288807fc67be037ff04f972ba7fa3f4aa91b441
smrangara12 <125582092+smrangara12@users.noreply.github.com>
2025-08-02 14:27:43 -0700

    Update sat_loan_terms.sql

--- models/lending/sat_loan_terms.sql
+++ models/lending/sat_loan_terms.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='incremental') }}
 
 select
-    {{ generate_hub_key('loan_id') }} as loan_hk,
+    {{ dbt_utils.generate_surrogate_key('loan_id') }} as loan_hk,
     interest_rate,
     principal_amount,
     disbursement_date,
@@ -11,4 +11,5 @@ select
     current_timestamp as load_date,
     '{{ invocation_id }}' as record_source
 from {{ ref('stg_corebank__loans') }}
+
 
