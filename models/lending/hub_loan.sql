edeb2ee33b1db7c4f3292c7e61a402d544f8a842
smrangara12 <125582092+smrangara12@users.noreply.github.com>
2025-08-02 15:55:40 -0700

    Update hub_loan.sql

--- models/lending/hub_loan.sql
+++ models/lending/hub_loan.sql
@@ -1,11 +1,12 @@
 {{ config(materialized='incremental') }}
 
 select
-    {{ dbt_utils.generate_surrogate_key('loan_id') }} as loan_hk,
+    {{ dbt_utils.generate_surrogate_key(['loan_id']) }} as loan_hk,
     loan_id,
     current_timestamp as load_date,
     'test1' as record_source
 from {{ ref('stg_corebank_loans') }}
+
 
 
 
