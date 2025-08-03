501e08360362be2622c86deb89c7d79797ed06e7
smrangara12 <125582092+smrangara12@users.noreply.github.com>
2025-08-03 13:26:37 -0700

    Create link_account_customer.sql

---
+++ models/lending/link_account_customer.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='incremental') }}
+
+select
+    {{ dbt_utils.generate_surrogate_key(['account_id', 'customer_id']) }} as account_customer_link_hk,
+    {{ dbt_utils.generate_surrogate_key(['account_id']) }} as account_hk,
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_hk,
+    current_timestamp as load_date,
+    'deposit_source' as record_source
+from {{ ref('stg_corebank_deposits') }}
